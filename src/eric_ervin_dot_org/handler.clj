0ab9b4e9d2fda330fd6f2c62334561f8c3173c1a
ericcervin <ericcervin@gmail.com>
2018-01-30 18:03:52 -0500

    added representation.clj . started moving formatting there.

--- src/eric_ervin_dot_org/handler.clj
+++ src/eric_ervin_dot_org/handler.clj
@@ -4,25 +4,15 @@
             [compojure.core :refer [defroutes ANY GET OPTIONS]]
             [hiccup.core :refer [html]]
             [clojure.java.jdbc :as sql]
+            [eric-ervin-dot-org.representation :refer [html-style-css]]
             [eric-ervin-dot-org.routes.discogs   :refer [discogs-routes]]
             [eric-ervin-dot-org.routes.destiny   :refer [destiny-routes]]
             [eric-ervin-dot-org.routes.gematria   :refer [gematria-routes]]
             [eric-ervin-dot-org.routes.philosophy-usa   :refer [philosophy-routes]]
             [eric-ervin-dot-org.routes.powerball :refer [powerball-routes]]
             [eric-ervin-dot-org.routes.serialism :refer [serialism-routes]]))
-            
 
-(def html-style-css [:style "table,th,td {
-                                          border: 1px solid black;
-                                          border-collapse: collapse;
-                                          padding: 3px;
-                                          text-align: center
-                                         }
-                             td {text-align: left}"])
-
-
-(defresource echo_context [ctx]
-                           
+(defresource echo_context [ctx]                           
              :allowed-methods [:get :options]
              :available-media-types ["text/plain"]
              :handle-ok (fn [ctx] (str ctx)))
@@ -53,12 +43,8 @@
                                            [:tr   [:td [:a {:href "/discogs"} "Discogs"][:td "Albums I've cataloged"][:td "12/16/2017"]]]
                                            [:tr   [:td [:a {:href "/gematria"} "Gematria"]][:td "The numerical value of words"][:td "N/A"]]
                                            [:tr   [:td [:a {:href "/philosophy"} "Philosophy"][:td "Philosophy degrees completed during the 2014-2015 academic year"][:td "12/23/2017"]]] ;]]])))
-                                           [:tr   [:td [:a {:href "/powerball"} "Powerball"]][:td " A source for Powerball numbers to play"][:td "N/A"]]]]])))
-                                           
-                                           
-                                    
-                                         
-
+                                           [:tr   [:td [:a {:href "/powerball"} "Powerball"]][:td " A source for Powerball numbers to play"][:td "N/A"]]
+                                           ]]])))
 (defroutes app-routes
   (ANY "/" [] root)
   
---
+++ src/eric_ervin_dot_org/representation.clj
@@ -0,0 +1,15 @@
+(ns eric-ervin-dot-org.representation
+  (:require [liberator.core :refer [resource defresource]]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer [defroutes ANY GET OPTIONS]]
+            [hiccup.core :refer [html]]
+            [clojure.java.jdbc :as sql]))
+
+(def html-style-css [:style "table,th,td {
+                               border: 1px solid black;
+                               border-collapse: collapse;
+                               padding: 3px;
+                               text-align: center
+                               }
+                             td {text-align: left}"])
+            
