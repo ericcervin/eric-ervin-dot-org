fe1da4e0f568aef518f3d66fa1e2a77c9c6db0ee
ericcervin <ericcervin@gmail.com>
2018-02-05 21:42:45 -0500

    fixed inversion in serialism. moved to front page.

--- src/eric_ervin_dot_org/routes/serialism.clj
+++ src/eric_ervin_dot_org/routes/serialism.clj
@@ -6,7 +6,17 @@
             [hiccup.page :refer [doctype html5]]
             [clojure.java.jdbc :as sql]
             [eric-ervin-dot-org.representation :refer [html-style-css map-html-table-td map-html-table-tr]]))
-  
+
+
+(defn random-dodeca-row [] (vec (shuffle (range 12))))
+
+(defn absolute-pitch-class [pc] (if (< pc 0) (+ pc 12) pc))
+
+(defn shift-to-zero [rw]
+     (let [old-row rw
+           or0 (rw 0)
+           new-row (map absolute-pitch-class (map #(- % or0) old-row))]
+      new-row))
 
 (defn serialism-html [mp]
   (html5  {:lang "en"} 
@@ -18,7 +28,7 @@
                   [:tr [:th "I0"](map map-html-table-td (map #(format "%02d" %) (:I0 mp)))]
                   [:tr [:th "RI0"](map map-html-table-td (map #(format "%02d" %) (:RI0 mp)))]]]))
 
-(defn serialism-map [] (let [P0 (vec (shuffle (range 12)))
+(defn serialism-map [] (let [P0 (vec (shift-to-zero (random-dodeca-row)))
                              R0 (vec (reverse P0))
                              I0 (vec(map #(if (= % 0) 0 (- 12 %)) P0))
                              RI0 (vec(reverse I0))]
