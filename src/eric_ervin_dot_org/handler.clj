bb6c8f272a67f0229585f3f26f936e6470631f63
ericcervin <ericcervin@gmail.com>
2018-02-11 00:57:03 -0500

    replaced hiccup with cljstache in handler.clj

--- src/eric_ervin_dot_org/handler.clj
+++ src/eric_ervin_dot_org/handler.clj
@@ -2,16 +2,21 @@
   (:require [liberator.core :refer [resource defresource]]
             [ring.middleware.params :refer [wrap-params]]
             [compojure.core :refer [defroutes ANY GET OPTIONS]]
-            [hiccup.core :refer [html]]
-            [hiccup.page :refer [doctype html5]]
+            ;;[hiccup.core :refer [html]]
+            ;;[hiccup.page :refer [doctype html5]]
+            [cljstache.core :refer [render]]
             [clojure.java.jdbc :as sql]
-            [eric-ervin-dot-org.representation :refer [html-style-css]]
+            ;;[eric-ervin-dot-org.representation :refer [html-style-css]]
             [eric-ervin-dot-org.routes.discogs   :refer [discogs-routes]]
             [eric-ervin-dot-org.routes.destiny   :refer [destiny-routes]]
             [eric-ervin-dot-org.routes.gematria   :refer [gematria-routes]]
             [eric-ervin-dot-org.routes.philosophy-usa   :refer [philosophy-routes]]
             [eric-ervin-dot-org.routes.powerball :refer [powerball-routes]]
-            [eric-ervin-dot-org.routes.serialism :refer [serialism-routes]]))
+            [eric-ervin-dot-org.routes.serialism :refer [serialism-routes]]
+            [eric-ervin-dot-org.routes.test :refer [test-routes]]))
+
+
+
 
 (defresource echo_context [ctx]                           
              :allowed-methods [:get :options]
@@ -24,32 +29,56 @@
              :handle-ok (fn [ctx] (:request ctx)))
 
 
+(def resource-list {:resources [{:name "Destiny"    :path "/destiny"    :last-updated "02/07/2018" :desc "Star Wars Destiny card game data"}
+                                {:name "Discogs"    :path "/discogs"    :last-updated "12/16/2017" :desc "Albums I've cataloged"}
+                                {:name "Gematria"   :path "/gematria"   :last-updated "N/A" :desc "The numerical value of words"}
+                                {:name "Philosophy" :path "/philosophy" :last-updated "12/23/2017" :desc "Philosophy degrees completed during the 2014-2015 academic year"}
+                                {:name "Powerball"  :path "/powerball"  :last-updated "N/A" :desc "A source for Powerball numbers to play"}
+                                {:name "Serialism"  :path "/serialism"  :last-updated "N/A" :desc "Toying with set theory and dodecaphony"}]})
+                     
+
+(def root-string
+  
+  "
+  <!DOCTYPE html>
+  <html lang=\"en\">
+  <head>
+  <title>Eric Ervin Dot Org</title>
+  <style>table,th,td {
+                               border: 1px solid black;
+                               border-collapse: collapse;
+                               padding: 3px;
+                               text-align: center
+                               }
+                             td {text-align: left}</style>
+  </head>
+  <body>
+  <div id=\"header\">
+  <h1>Eric Ervin Dot Org</h1>
+  <p>A toy website to release some Clojure into the world.</p>
+  <p>Though it's a toy, these are resources I use. (Except that I've quit playing Powerball)</p>
+  <p><a href=\"https://github.com/ericcervin/eric-ervin-dot-org\">https://github.com/ericcervin/eric-ervin-dot-org</a></p>
+  <br>
+  </div>
+  <div id=\"resources\">
+  <table>
+  <thead><tr><th scope=\"col\">Resource</th><th scope=\"col\">Description</th><th scope=\"col\">Data Updated</th></tr></thead>
+  <tbody>
+  {{#resources}}
+  <tr><td><a href=\"{{path}}\">{{name}}</a></td><td>{{desc}}</td><td>{{last-updated}}</td></tr>
+  {{/resources}}
+  </tbody>
+  </table>
+  </div>
+  </body>
+  </html>
+  ")
+
 (defresource root [ctx]
              :allowed-methods [:get :options]
              :available-media-types ["text/html"]
-             :handle-ok (fn [ctx] (html5  {:lang "en"}
-                                          [:head html-style-css] 
-                                     
-                                          [:body
-                                           [:div {:id "header"}
-                                            [:h1 "Eric Ervin Dot Org"]
-                                            [:p "A toy website to release some Clojure into the world."]
-                                            [:p "Though it's a toy, these are resources I use. (Except that I've quit playing Powerball)"]
-                                            [:p [:a {:href "https://github.com/ericcervin/eric-ervin-dot-org"} "https://github.com/ericcervin/eric-ervin-dot-org"]][:br]]
-                                           
-                                           
-                                           [:div {:id "resources"}
-                                            [:table
-                                             [:thead 
-                                              [:tr   [:th {:scope "col"} "Resource"] [:th {:scope "col"}"Description"][:th {:scope "col"} "Data Updated"]]]                                  
-                                             [:tbody
-                                              [:tr   [:td [:a {:href "/destiny"} "Destiny"]][:td "Star Wars Destiny card game data"][:td "02/07/2018"]]
-                                              [:tr   [:td [:a {:href "/discogs"} "Discogs"]][:td "Albums I've cataloged"][:td "12/16/2017"]]
-                                              [:tr   [:td [:a {:href "/gematria"} "Gematria"]][:td "The numerical value of words"][:td "N/A"]]
-                                              [:tr   [:td [:a {:href "/philosophy"} "Philosophy"]][:td "Philosophy degrees completed during the 2014-2015 academic year"][:td "12/23/2017"]] ;]]])))
-                                              [:tr   [:td [:a {:href "/powerball"} "Powerball"]][:td "A source for Powerball numbers to play"][:td "N/A"]]
-                                              [:tr   [:td [:a {:href "/serialism"} "Serialism"]][:td "Toying with set theory and dodecaphony"][:td "N/A"]]]]]])))
-                                           
+             :handle-ok (render root-string resource-list))
+
 (defroutes app-routes
   (ANY "/" [] root)
   
@@ -67,7 +96,9 @@
   
   powerball-routes
   
-  serialism-routes)
+  serialism-routes
+  
+  test-routes)
   
 (def app
   (wrap-params app-routes))
