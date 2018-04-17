223d0892c2f3c385dc68e7d585e181cb633a631e
ericcervin <ericcervin@gmail.com>
2018-04-17 18:15:05 -0400

    added ids and formatting in gematria to aid tests

--- src/eric_ervin_dot_org/routes/gematria.clj
+++ src/eric_ervin_dot_org/routes/gematria.clj
@@ -8,29 +8,32 @@
 
 
 (def gematria-root-template "<!DOCTYPE html>
-    <html lang=\"en\"><head><style>table,th,td {
-                               border: 1px solid black;
-                               border-collapse: collapse;
-                               padding: 3px;
-                               text-align: center
-                               }
-                             td {text-align: left}</style>
+    <html lang=\"en\">
+    <head>
+    <title>Gematria</title>
+    <style>table,th,td {
+                 border: 1px solid black;
+                 border-collapse: collapse;
+                 padding: 3px;
+                 text-align: center
+                 }
+           td {text-align: left}</style>
     </head>
     <body>
     <div id=\"header\">
-    <h2>Gematria</h2>
+    <h1>Gematria</h1>
     <p>See also:<a href=\"https://en.wikipedia.org/wiki/Gematria\">Wikipedia</a></p>
     </div>
     <div id=\"word_form\">
     <p>Calculate the numerical value of a word.</p>
     <form action=\"/gematria/word\" method=\"get\">
-    <input name=\"word\" type=\"text\">
+    <input id=\"id_word_input\" name=\"word\" type=\"text\">
     <input type=\"submit\" value=\"Calculate\"></form>
     </div>
     <div id=\"value_form\">
     <p>Search the 10,000 most common English words by numerical value.</p>
     <form action=\"/gematria/value\" method=\"get\">
-    <input name=\"value\" type=\"text\">
+    <input id=\"id_value_input\" name=\"value\" type=\"text\">
     <input type=\"submit\" value=\"Search\">
     </form>
     </div>
