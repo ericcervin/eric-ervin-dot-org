83e24ef8e35c2449357c027c3cb4fe25ca9f9ad6
ericcervin <ericcervin@gmail.com>
2018-05-03 16:05:12 -0400

    added root page for serialism route

--- src/eric_ervin_dot_org/routes/serialism.clj
+++ src/eric_ervin_dot_org/routes/serialism.clj
@@ -6,29 +6,64 @@
             [clojure.java.jdbc :as sql]))
             ;;[eric-ervin-dot-org.representation :refer [html-style-css map-html-table-td map-html-table-tr]]))
 
+
+(def serialism-root-template "
+  <!DOCTYPE html>  
+  <html lang=\"en\">            
+      <head>
+        <style>
+           table,th,td {
+             border: 1px solid black;
+             border-collapse: collapse;
+             padding: 3px;
+             text-align: center
+            }
+             td {text-align: left}
+        </style>
+        <title>Serialism</title>
+      </head>
+      <body>
+        <div id=\"header\">
+          <h3>Serialism</h3>
+          <p>Rows and rows of numbers</p>
+        </div>
+        <div id=\"numbers\">
+          <table>
+            <thead>
+              <tr><th scope=\"col\">Numbers</th></tr>
+            </thead>
+            <tbody>
+              <tr><td><a href=\"/serialism/html\">HTML</a></td></tr>
+            </tbody>
+          </table>
+        </div>
+      </body>
+    </html>")
+
+
 (def serialism-result-template "
-<!DOCTYPE html>
-<html lang=\"en\">
-<head>  
-  <style> table,th,td {
-                border: 1px solid black;
-                border-collapse: collapse;
-                padding: 3px;
-                text-align: center;}
-           td {text-align: left}
-  </style>
-  <title>Serialism</title>
-  </head>
-  <body>
-  <table>
-  <tbody>
-  <tr><th>P0</th>{{#p0}}<td>{{.}}</td>{{/p0}}</tr>
-  <tr><th>R0</th>{{#r0}}<td>{{.}}</td>{{/r0}}</tr>
-  <tr><th>I0</th>{{#i0}}<td>{{.}}</td>{{/i0}}</tr>
-  <tr><th>RI0</th>{{#ri0}}<td>{{.}}</td>{{/ri0}}</tr>
-  </tbody>
-  </table>
-  </body>
+  <!DOCTYPE html>
+  <html lang=\"en\">
+    <head>  
+      <style> table,th,td {
+         border: 1px solid black;
+         border-collapse: collapse;
+         padding: 3px;
+         text-align: center;}
+         td {text-align: left}
+      </style>
+      <title>Serialism</title>
+    </head>
+    <body>
+      <table>
+        <tbody>
+          <tr><th>P0</th>{{#p0}}<td>{{.}}</td>{{/p0}}</tr>
+          <tr><th>R0</th>{{#r0}}<td>{{.}}</td>{{/r0}}</tr>
+          <tr><th>I0</th>{{#i0}}<td>{{.}}</td>{{/i0}}</tr>
+          <tr><th>RI0</th>{{#ri0}}<td>{{.}}</td>{{/ri0}}</tr>
+        </tbody>
+      </table>
+    </body>
   </html>")
 
 (defn random-dodeca-row [] (vec (shuffle (range 12))))
@@ -54,9 +89,15 @@
 (defresource res-serialism [ctx]
              :allowed-methods [:get :options]
              :available-media-types ["text/html"]
+             :handle-ok (render serialism-root-template))
+
+(defresource res-serialism-html [ctx]
+             :allowed-methods [:get :options]
+             :available-media-types ["text/html"]
              :handle-ok (render serialism-result-template (serialism-map)))
 
 (defroutes serialism-routes
-  (ANY "/serialism" [] res-serialism))
+  (ANY "/serialism" [] res-serialism)
+  (ANY "/serialism/html" [] res-serialism-html))
 
 
