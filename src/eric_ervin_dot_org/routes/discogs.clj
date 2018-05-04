c0736860109e260ab6390ffefe98862cb1b5a67d
ericcervin <ericcervin@gmail.com>
2018-05-04 17:04:16 -0400

    improved metadata and formatting in discogs route

--- src/eric_ervin_dot_org/routes/discogs.clj
+++ src/eric_ervin_dot_org/routes/discogs.clj
@@ -11,27 +11,26 @@
 (def discogs-report-template 
      "<!DOCTYPE html>
       <html lang=\"en\">
-      <head>
-      <style> table,th,td {
-                      border: 1px solid black;
-                      border-collapse: collapse;
-                      padding: 3px;
-                      text-align: center;}
-                               
-              td {text-align: left}
-      </style>
-      </head>
-      <body>
-      <table>
-      <thead><tr>{{#header}}<th>{{.}}</th>{{/header}}</tr></thead>
-      <tbody>
-      {{#results}}
-      <tr>{{#result}}<td>{{{vl}}}</td>{{/result}}</tr>
-      {{/results}}
-      </tbody>
-      </table>
-      </body>
-      </html>")
+        <head>
+          <style> table,th,td {
+                  border: 1px solid black;
+                  border-collapse: collapse;
+                  padding: 3px;
+                  text-align: center;}
+                  td {text-align: left}
+          </style>
+       </head>
+       <body>
+         <table>
+           <thead><tr>{{#header}}<th>{{.}}</th>{{/header}}</tr></thead>
+           <tbody>
+           {{#results}}
+           <tr>{{#result}}<td>{{{vl}}}</td>{{/result}}</tr>
+           {{/results}}
+           </tbody>
+         </table>
+       </body>
+     </html>")
 
 (defn vector-of-maps [x] (vec (map #(hash-map :vl %) x)))
 
@@ -47,49 +46,48 @@
 (def discogs-root-template "
      <!DOCTYPE html>
      <html lang=\"en\">
-     <head>
-     <style> table,th,td {
-                      border: 1px solid black;
-                      border-collapse: collapse;
-                      padding: 3px;
-                      text-align: center;}
-                               
-              td {text-align: left}
-     </style>
-     </head>
-     <body>
-     <div id=\"header\"><h3>My Record Collection</h3>
-     </div>
-     <div id=\"releases\">
-     <h4>Releases</h4>
-     <table>
-     <thead>
-     <tr>
-     <th></th>
-     <th scope=\"col\">By Title</th>
-     <th scope=\"col\">By Artist</th>
-     <th scope=\"col\">By Label</th>
-     <th scope=\"col\">By Release Year</th></tr>
-     </thead>
-     <tbody>
-     <tr>
-     <th>All</th><td><a href=\"/discogs/releases?sort=title\">HTML</a></td>
-     <td><a href=\"/discogs/releases?sort=artist\">HTML</a></td>
-     <td><a href=\"/discogs/releases?sort=label\">HTML</a></td>
-     <td><a href=\"/discogs/releases?sort=year\">HTML</a></td></tr>
-     </tbody>
-     </table>
-     </div>
-     <div id=\"reports\"><h4>Reports</h4>
-     <table>
-     <thead><tr>
-     <th scope=\"col\">Report</th><th scope=\"col\">Format</th></tr></thead>
-     <tbody>
-     <tr><td>Count by Artist</td><td><a href=\"/discogs/reports?rpt=artist_count\">HTML</a></td></tr>
-     <tr><td>Count by Label</td><td><a href=\"/discogs/reports?rpt=label_count\">HTML</a></td></tr>
-     <tr><td>Count by Year Released</td><td><a href=\"/discogs/reports?rpt=year_count\">HTML</a></td></tr>
-     </tbody>
-     </table></div></body></html>")
+       <head>
+         <title>Discogs</title>
+         <style> table,th,td {
+                 border: 1px solid black;
+                 border-collapse: collapse;
+                 padding: 3px;
+                 text-align: center;}              
+                 td {text-align: left}
+         </style>
+       </head>
+       <body>
+         <div id=\"header\"><h1>My Record Collection</h1></div>
+         <div id=\"releases\">
+         <h4>Releases</h4>
+         <table>
+           <thead>
+            <tr>
+            <th></th>
+            <th scope=\"col\">By Title</th>
+            <th scope=\"col\">By Artist</th>
+            <th scope=\"col\">By Label</th>
+            <th scope=\"col\">By Release Year</th></tr>
+          </thead>
+          <tbody>
+            <tr>
+            <th>All</th><td><a href=\"/discogs/releases?sort=title\">HTML</a></td>
+            <td><a href=\"/discogs/releases?sort=artist\">HTML</a></td>
+            <td><a href=\"/discogs/releases?sort=label\">HTML</a></td>
+            <td><a href=\"/discogs/releases?sort=year\">HTML</a></td></tr>
+          </tbody>
+       </table>
+       </div>
+       <div id=\"reports\"><h4>Reports</h4>
+      <table>
+        <thead><tr>
+        <th scope=\"col\">Report</th><th scope=\"col\">Format</th></tr></thead>
+        <tbody>
+          <tr><td>Count by Artist</td><td><a href=\"/discogs/reports?rpt=artist_count\">HTML</a></td></tr>
+          <tr><td>Count by Label</td><td><a href=\"/discogs/reports?rpt=label_count\">HTML</a></td></tr>
+          <tr><td>Count by Year Released</td><td><a href=\"/discogs/reports?rpt=year_count\">HTML</a></td></tr>
+        </tbody>
+      </table></div></body></html>")
   
 
 (defn releases-query [ctx] 
