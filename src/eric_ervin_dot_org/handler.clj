7a5181346973b7065046016c33615376c4b4f028
ericcervin <ericcervin@gmail.com>
2018-07-03 19:41:34 -0400

    updated destiny.db

--- src/eric_ervin_dot_org/handler.clj
+++ src/eric_ervin_dot_org/handler.clj
@@ -36,7 +36,7 @@
              :handle-ok (fn [ctx] (get-in ctx [:request :route-params :etc])))
 
 
-(def resource-list {:resources [{:name "Destiny"    :path "/destiny"    :last-updated "06/25/2018" :desc "Star Wars Destiny card game data"}
+(def resource-list {:resources [{:name "Destiny"    :path "/destiny"    :last-updated "07/03/2018" :desc "Star Wars Destiny card game data"}
                                 {:name "Discogs"    :path "/discogs"    :last-updated "12/16/2017" :desc "Albums I've cataloged"}
                                 {:name "Gematria"   :path "/gematria"   :last-updated "N/A" :desc "The numerical value of words"}
                                 {:name "Philosophy" :path "/philosophy" :last-updated "12/23/2017" :desc "Philosophy degrees completed during the 2014-2015 academic year"}
