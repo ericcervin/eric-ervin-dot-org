29153a0b94749b6fd067db7509c3255b6ab972dd
ericcervin <ericcervin@gmail.com>
2018-10-04 17:46:39 -0400

    updated discogs.db

--- src/eric_ervin_dot_org/handler.clj
+++ src/eric_ervin_dot_org/handler.clj
@@ -37,7 +37,7 @@
 
 
 (def resource-list {:resources [{:name "Destiny"    :path "/destiny"    :last-updated "09/13/2018" :desc "Star Wars Destiny card game data"}
-                                {:name "Discogs"    :path "/discogs"    :last-updated "12/16/2017" :desc "Albums I've cataloged"}
+                                {:name "Discogs"    :path "/discogs"    :last-updated "10/04/2018" :desc "Albums I've cataloged"}
                                 {:name "Gematria"   :path "/gematria"   :last-updated "N/A" :desc "The numerical value of words"}
                                 {:name "Philosophy" :path "/philosophy" :last-updated "12/23/2017" :desc "Philosophy degrees completed during the 2014-2015 academic year"}
                                 {:name "Powerball"  :path "/powerball"  :last-updated "N/A" :desc "A source for Powerball numbers to play"}
